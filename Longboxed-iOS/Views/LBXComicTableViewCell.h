a720aebc7ce06033ad80ea15c6b699ae2f9e1508
jayhickey <johnrhickey@gmail.com>
2014-10-01 21:29:07 -0400

    Added view between table views for 6 Plus

---
+++ Longboxed-iOS/Views/LBXComicTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  LBXComicTableViewCell.h
+//  Longboxed-iOS
+//
+//  Created by johnrhickey on 9/14/14.
+//  Copyright (c) 2014 Longboxed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LBXComicTableViewCell : UITableViewCell
+
+@property (nonatomic, weak) IBOutlet UIImageView *backgroundImageView;
+@property (nonatomic, weak) IBOutlet UILabel *titleLabel;
+
+@end
