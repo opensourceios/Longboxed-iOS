4b1d19986428425f3d16f92aa29800b5e31e6909
jayhickey <johnrhickey@gmail.com>
2014-11-07 00:33:11 -0500

    Added crashlytics logging

--- Longboxed-iOS/Abstract/LBXLogging.m
+++ Longboxed-iOS/Abstract/LBXLogging.m
@@ -9,27 +9,32 @@
 #import "LBXLogging.h"
 #import "DDLog.h"
 
+#import <Crashlytics/Crashlytics.h>
 #import <UICKeyChainStore.h>
 
 @implementation LBXLogging
 
 + (void)beginLogging
 {
+    CLS_LOG(@"\nStarted logging for %@ (ID: %@)\n===========\n", [UICKeyChainStore stringForKey:@"username"], [UICKeyChainStore stringForKey:@"id"]);
     DDLogInfo(@"\nStarted logging for %@ (ID: %@)\n===========\n", [UICKeyChainStore stringForKey:@"username"], [UICKeyChainStore stringForKey:@"id"]);
 }
 
 + (void)logLogin
 {
+    CLS_LOG(@"\nLogged in with username: %@ (ID: %@)\n===========\n", [UICKeyChainStore stringForKey:@"username"], [UICKeyChainStore stringForKey:@"id"]);
     DDLogInfo(@"\nLogged in with username: %@ (ID: %@)\n===========\n", [UICKeyChainStore stringForKey:@"username"], [UICKeyChainStore stringForKey:@"id"]);
 }
 
 + (void)logLogout
 {
+    CLS_LOG(@"\nLogged out username: %@ (ID: %@)\n===========\n", [UICKeyChainStore stringForKey:@"username"], [UICKeyChainStore stringForKey:@"id"]);
     DDLogInfo(@"\nLogged out username: %@ (ID: %@)\n===========\n", [UICKeyChainStore stringForKey:@"username"], [UICKeyChainStore stringForKey:@"id"]);
 }
 
 + (void)logMessage:(NSString *)message
 {
+    CLS_LOG(@"\n%@\n(Username: %@, User ID: %@)\n===========\n", message, [UICKeyChainStore stringForKey:@"username"], [UICKeyChainStore stringForKey:@"id"]);
     DDLogInfo(@"\n%@\n(Username: %@, User ID: %@)\n===========\n", message, [UICKeyChainStore stringForKey:@"username"], [UICKeyChainStore stringForKey:@"id"]);
 }
 
