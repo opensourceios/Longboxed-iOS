e327314c5508ce8fd53b3c670ded47ffa87ed658
jayhickey <johnrhickey@gmail.com>
2015-02-09 22:38:49 -0500

    Added in app purchase functionality

--- Longboxed-iOS/Controllers/LBXSettingsViewController.m
+++ Longboxed-iOS/Controllers/LBXSettingsViewController.m
@@ -35,8 +35,9 @@
 #import <WebKit/WebKit.h>
 
 #import <Crashlytics/Crashlytics.h>
+#import <StoreKit/StoreKit.h>
 
-@interface LBXSettingsViewController () <UITableViewDelegate, UITableViewDataSource, MFMailComposeViewControllerDelegate>
+@interface LBXSettingsViewController () <UITableViewDelegate, UITableViewDataSource, MFMailComposeViewControllerDelegate, SKProductsRequestDelegate, SKPaymentTransactionObserver>
 
 @property (nonatomic, strong) UISwitch *developmentServerSwitch;
 @property (nonatomic, strong) IBOutlet UITableView* settingsTableView;
@@ -139,19 +140,95 @@ UICKeyChainStore *store;
     [self.navigationController dismissViewControllerAnimated:YES completion:nil];
 }
 
+# pragma mark In-App Purchase
+
+- (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response {
+    SKProduct *validProduct = nil;
+    NSUInteger count = [response.products count];
+    if(count > 0){
+        validProduct = [response.products objectAtIndex:0];
+        [LBXLogging logMessage:[NSString stringWithFormat:@"Triggered in app purchase of $%@", validProduct.price]];
+        [self purchase:validProduct];
+    }
+    else if(!validProduct){
+        [SVProgressHUD dismiss];
+        [LBXControllerServices showAlertWithTitle:@"Unable to Connect to Apple Servers" andMessage:@"Longboxed is unable to retrieve the in app purchases from iTunes. Please try again later."];
+    }
+}
+
+- (IBAction)purchase:(SKProduct *)product{
+    SKPayment *payment = [SKPayment paymentWithProduct:product];
+    [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
+    [[SKPaymentQueue defaultQueue] addPayment:payment];
+}
+
+- (void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray *)transactions{
+    for(SKPaymentTransaction *transaction in transactions){
+        switch (transaction.transactionState){
+            case SKPaymentTransactionStatePurchasing: [LBXLogging logMessage:@"Purchasing..."];
+                //called when the user is in the process of purchasing, do not add any of your own code here.
+                break;
+            case SKPaymentTransactionStatePurchased:
+                [SVProgressHUD dismiss];
+                //this is called when the user has successfully purchased the package (Cha-Ching!)
+                [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+                [LBXLogging logMessage:@"In-app purchase made!"];
+                break;
+            case SKPaymentTransactionStateRestored:
+                [SVProgressHUD dismiss];
+                [LBXLogging logMessage:@"Restored in app purchase"];
+                [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+                break;
+            case SKPaymentTransactionStateFailed:
+                [SVProgressHUD dismiss];
+                if(transaction.error.code != SKErrorPaymentCancelled){
+                    [LBXLogging logMessage:@"Failed/cancelled in app purchase"];
+                }
+                [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+                break;
+            case SKPaymentTransactionStateDeferred:
+                [SVProgressHUD dismiss];
+                [LBXLogging logMessage:@"Deferred payment via Ask to Buy"];
+                break;
+        }
+    }
+}
+
 - (IBAction)sendTip:(id)sender
 {
-    UIButton *button = (UIButton *)sender;
-    switch ([button tag]) {
-        case 0:
-            [LBXControllerServices showAlertWithTitle:@"Small Tip" andMessage:@"This is a small tip"];
-            break;
-        case 1:
-            [LBXControllerServices showAlertWithTitle:@"Medium Tip" andMessage:@"This is a medium tip"];
-            break;
-        case 2:
-            [LBXControllerServices showAlertWithTitle:@"Big Tip" andMessage:@"This is a big tip"];
-            break;
+    if([SKPaymentQueue canMakePayments]) {
+        // Load in app purchase identifiers from json file
+        NSMutableDictionary *identifierDict = [NSMutableDictionary new];
+        NSString *path = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"InAppPurchase.json"];
+        if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
+            identifierDict = [NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfFile:path]
+                                                              options:kNilOptions
+                                                                error:nil];
+        }
+        
+        NSString *identifierString = [NSString new];
+        
+        UIButton *button = (UIButton *)sender;
+        switch ([button tag]) {
+            case 0:
+                identifierString = identifierDict[@"Small Tip"];
+                break;
+            case 1:
+                identifierString = identifierDict[@"Medium Tip"];
+                break;
+            case 2:
+                identifierString = identifierDict[@"Large Tip"];
+                break;
+        }
+        
+        [SVProgressHUD showWithStatus:@"Tip In Progress"];
+        SKProductsRequest *productsRequest = [[SKProductsRequest alloc] initWithProductIdentifiers:[NSSet setWithObject:identifierString]];
+        productsRequest.delegate = self;
+        [productsRequest start];
+    }
+    else{
+        [SVProgressHUD dismiss];
+        [LBXControllerServices showAlertWithTitle:@"Unable to Purchase" andMessage:@"You are unable to perform in app purchases. This is likely due to parental controls."];
     }
 }
 
